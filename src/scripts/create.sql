b743a414284bf79c7ca3dce1bd25fee6ccb2ade4
Kyoto67 <whynot.2@icloud.com>
2023-10-12 19:43:33 +0300

    rework models

--- src/scripts/create.sql
+++ src/scripts/create.sql
@@ -2,37 +2,48 @@ CREATE TYPE sex AS ENUM ('male', 'female', 'both');
 
 CREATE TABLE actions (
     action_id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE feels (
     feel_id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE groups (
-    group_id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
+CREATE TABLE actor_categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
+
 
 CREATE TABLE actors (
     actor_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     sex sex,
-    group_id INT REFERENCES groups(group_id)
+    category INT REFERENCES actor_categories(category_id)
+);
+
+CREATE TABLE actor_category_history (
+    history_id SERIAL PRIMARY KEY,
+    actor INT NOT NULL,
+    category INT NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    FOREIGN KEY (actor) REFERENCES actors(actor_id),
+    FOREIGN KEY (category) REFERENCES actor_categories(category_id)
 );
 
 CREATE TABLE events (
     event_id SERIAL PRIMARY KEY,
-    actor INT REFERENCES actors(actor_id),
-    action INT REFERENCES actions(action_id),
+    actor INT REFERENCES actors(actor_id) NOT NULL,
+    action INT REFERENCES actions(action_id) NOT NULL,
     timestamp_from BIGINT,
     timestamp_to BIGINT
 );
 
 CREATE TABLE expressions (
     expression_id SERIAL PRIMARY KEY,
-    who INT REFERENCES actors(actor_id),
-    to_whom INT REFERENCES actors(actor_id),
-    feel INT REFERENCES feels(feel_id)
+    who INT REFERENCES actors(actor_id) NOT NULL,
+    to_whom INT REFERENCES actors(actor_id) NOT NULL,
+    feel INT REFERENCES feels(feel_id) NOT NULL
 );
--- src/scripts/insert.sql
+++ src/scripts/insert.sql
@@ -1,25 +1,39 @@
-INSERT INTO groups (name)
+INSERT INTO actor_categories (name)
 VALUES
     ('взрослые'),
     ('дети'),
-    ('родители'),
+    ('динозавры'),
     ('экспонаты');
 
-INSERT INTO actors (name, sex, group_id)
+INSERT INTO actors (name, sex, category)
 VALUES
-    ('элли', 'female', (SELECT group_id FROM groups WHERE name = 'взрослые')),
-    ('малкольм', 'male', (SELECT group_id FROM groups WHERE name = 'взрослые')),
-    ('грант', 'male', (SELECT group_id FROM groups WHERE name = 'взрослые')),
-    ('мальчик', 'male', (SELECT group_id FROM groups WHERE name = 'дети')),
-    ('дети', 'both', (SELECT group_id FROM groups WHERE name = 'дети')),
-    ('родители', 'both', (SELECT group_id FROM groups WHERE name = 'родители')),
-    ('динозавры', 'both', (SELECT group_id FROM groups WHERE name = 'экспонаты'));
+    ('элли', 'female', (SELECT category_id FROM actor_categories WHERE name = 'взрослые')),
+    ('малкольм', 'male', (SELECT category_id FROM actor_categories WHERE name = 'взрослые')),
+    ('грант', 'male', (SELECT category_id FROM actor_categories WHERE name = 'взрослые')),
+    ('мальчик', 'male', (SELECT category_id FROM actor_categories WHERE name = 'дети')),
+    ('ребенок', 'both', (SELECT category_id FROM actor_categories WHERE name = 'дети')),
+    ('родитель', 'both', (SELECT category_id FROM actor_categories WHERE name = 'взрослые')),
+    ('динозавр', 'both', (SELECT category_id FROM actor_categories WHERE name = 'экспонаты'));
+
+INSERT INTO actor_category_history (actor, category, start_date, end_date)
+VALUES
+    ( (SELECT actor_id FROM actors WHERE name = 'элли'), (SELECT category_id FROM actor_categories WHERE name = 'дети'), '1970-02-03', '1988-02-03'),
+    ( (SELECT actor_id FROM actors WHERE name = 'элли'), (SELECT category_id FROM actor_categories WHERE name = 'взрослые'), '1988-02-03', NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'малкольм'), (SELECT category_id FROM actor_categories WHERE name = 'дети'), '1978-03-12', '1996-03-12'),
+    ( (SELECT actor_id FROM actors WHERE name = 'малкольм'), (SELECT category_id FROM actor_categories WHERE name = 'взрослые'), '1996-03-12', NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'грант'), (SELECT category_id FROM actor_categories WHERE name = 'дети'), '1974-12-10', '1992-12-10'),
+    ( (SELECT actor_id FROM actors WHERE name = 'грант'), (SELECT category_id FROM actor_categories WHERE name = 'взрослые'), '1992-12-10', NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'мальчик'), (SELECT category_id FROM actor_categories WHERE name = 'дети'), '2000-12-10', NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT category_id FROM actor_categories WHERE name = 'дети'), NULL, NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'родитель'), (SELECT category_id FROM actor_categories WHERE name = 'взрослые'), NULL, NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'динозавр'), (SELECT category_id FROM actor_categories WHERE name = 'динозавры'), NULL, NULL),
+    ( (SELECT actor_id FROM actors WHERE name = 'динозавр'), (SELECT category_id FROM actor_categories WHERE name = 'экспонаты'), '1980-03-05', NULL);
 
 INSERT INTO feels (name)
 VALUES
     ('любовь'),
     ('интерес'),
-    ('производить впечатление'),
+    ('впечатляться'),
     ('обожание'),
     ('страх');
 
@@ -33,19 +47,19 @@ VALUES
 
 INSERT INTO expressions (who, to_whom, feel)
 VALUES
-    ((SELECT actor_id FROM actors WHERE name = 'грант'), (SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT feel_id FROM feels WHERE name = 'любовь')),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT actor_id FROM actors WHERE name = 'динозавры'), (SELECT feel_id FROM feels WHERE name = 'интерес')),
-    ((SELECT actor_id FROM actors WHERE name = 'динозавры'), (SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT feel_id FROM feels WHERE name = 'производить впечатление')),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT actor_id FROM actors WHERE name = 'динозавры'), (SELECT feel_id FROM feels WHERE name = 'любовь')),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT actor_id FROM actors WHERE name = 'родители'), (SELECT feel_id FROM feels WHERE name = 'обожание')),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT actor_id FROM actors WHERE name = 'родители'), (SELECT feel_id FROM feels WHERE name = 'страх')),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT actor_id FROM actors WHERE name = 'родители'), (SELECT feel_id FROM feels WHERE name = 'любовь'));
+    ((SELECT actor_id FROM actors WHERE name = 'грант'), (SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT feel_id FROM feels WHERE name = 'любовь')),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT actor_id FROM actors WHERE name = 'динозавр'), (SELECT feel_id FROM feels WHERE name = 'интерес')),
+    ((SELECT actor_id FROM actors WHERE name = 'динозавр'), (SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT feel_id FROM feels WHERE name = 'впечатляться')),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT actor_id FROM actors WHERE name = 'динозавр'), (SELECT feel_id FROM feels WHERE name = 'любовь')),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT actor_id FROM actors WHERE name = 'родитель'), (SELECT feel_id FROM feels WHERE name = 'обожание')),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT actor_id FROM actors WHERE name = 'родитель'), (SELECT feel_id FROM feels WHERE name = 'страх')),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT actor_id FROM actors WHERE name = 'родитель'), (SELECT feel_id FROM feels WHERE name = 'любовь'));
 
-INSERT INTO events (actor, action, order_or)
+INSERT INTO events (actor, action, timestamp_from, timestamp_to)
 VALUES
     ((SELECT actor_id FROM actors WHERE name = 'элли'), (SELECT action_id FROM actions WHERE name = 'обойти здание'), 1000, 2000),
     ((SELECT actor_id FROM actors WHERE name = 'малкольм'), (SELECT action_id FROM actions WHERE name = 'обойти здание'), 1000, 2000),
     ((SELECT actor_id FROM actors WHERE name = 'грант'), (SELECT action_id FROM actions WHERE name = 'обойти здание'), 1000, 2000),
     ((SELECT actor_id FROM actors WHERE name = 'мальчик'), (SELECT action_id FROM actions WHERE name = 'следовать'), 2000, 3000),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT action_id FROM actions WHERE name = 'стоять'), 3000, 4000),
-    ((SELECT actor_id FROM actors WHERE name = 'дети'), (SELECT action_id FROM actions WHERE name = 'смотреть'), 4000, 5000);
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT action_id FROM actions WHERE name = 'стоять'), 3000, 4000),
+    ((SELECT actor_id FROM actors WHERE name = 'ребенок'), (SELECT action_id FROM actions WHERE name = 'смотреть'), 4000, 5000);
